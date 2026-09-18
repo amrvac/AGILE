77fea32df92e4707ba1b1acfa4a472400a13e626
Leon Oostrum <l.oostrum@esciencecenter.nl>
2026-09-18 15:37:33 +0200

    Improve OpenMP offload support options in llvm arch

--- arch/llvm.mk
+++ arch/llvm.mk
@@ -1,13 +1,18 @@
 arch := llvm
 
-compile = flang
-f90_flags += -ffree-form -fimplicit-none -cpp $(shell mpifort --showme:compile)
+compile = mpif90
+f90_flags += -ffree-form -fimplicit-none -cpp
 
 ifdef OPENMP
 $(info Enabling OpenMP)
 enabled += OPENMP
-f90_flags += -fopenmp
+f90_flags += -fopenmp -fopenmp-version=52 --offload-arch=native
+ifdef NOGPUDIRECT
+$(info Disabling direct GPU-GPU copies)
+enabled += NOGPUDIRECT
+f90_flags += -DNOGPUDIRECT
+endif
 endif
 
-link_flags += $(f90_flags) $(shell mpifort --showme:link)
+link_flags += $(f90_flags)
 
